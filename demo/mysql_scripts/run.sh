e429982669d93eb857773d9faa71801b9b3a8ea4
liyancheng <412998149@qq.com>
2025-07-17 16:18:44 +0800

    [AutoPrefetch] Delete AutoPrefetch mode Since create_gcov does not support parser multiply pmu events, A-FOT can`t support AutoPrefetch mode anymore.

--- demo/mysql_scripts/run.sh
+++ demo/mysql_scripts/run.sh
@@ -9,10 +9,10 @@ echo 3 > /proc/sys/vm/drop_caches
 numactl -C 120-127 $mysql_install_path/bin/mysqld --default-file=/etc/my.cnf &
 sleep 15
 
-sshpass -p "PASSWD" ssh "CLIENT_IP" "sysbench --db-driver=mysql --mysql-host=HOST_IP --mysql-port=3306 --mysql-user=root --mysql-password=123456 --mysql-db=sbtest --table_size=10000000 --tables=64  --time=180  --threads=32 --report-interval=10 oltp_read_only run"
+sshpass -p "PASSWD" ssh "CLIENT_IP" "sysbench --db-driver=mysql --mysql-host=HOST_IP --mysql-port=3306 --mysql-user=root --mysql-password=PASSWD --mysql-db=sbtest --table_size=10000000 --tables=64  --time=180  --threads=32 --report-interval=10 oltp_read_only run"
 
-sshpass -p "PASSWD" ssh "CLIENT_IP" "sysbench --db-driver=mysql --mysql-host=HOST_IP --mysql-port=3306 --mysql-user=root --mysql-password=123456 --mysql-db=sbtest --table_size=10000000 --tables=64  --time=180  --threads=32 --report-interval=10 oltp_write_only run"
+sshpass -p "PASSWD" ssh "CLIENT_IP" "sysbench --db-driver=mysql --mysql-host=HOST_IP --mysql-port=3306 --mysql-user=root --mysql-password=PASSWD --mysql-db=sbtest --table_size=10000000 --tables=64  --time=180  --threads=32 --report-interval=10 oltp_write_only run"
 
-sshpass -p "PASSWD" ssh "CLIENT_IP" "sysbench --db-driver=mysql --mysql-host=HOST_IP --mysql-port=3306 --mysql-user=root --mysql-password=123456 --mysql-db=sbtest --table_size=10000000 --tables=64  --time=180  --threads=32 --report-interval=10 oltp_read_write run"
+sshpass -p "PASSWD" ssh "CLIENT_IP" "sysbench --db-driver=mysql --mysql-host=HOST_IP --mysql-port=3306 --mysql-user=root --mysql-password=PASSWD --mysql-db=sbtest --table_size=10000000 --tables=64  --time=180  --threads=32 --report-interval=10 oltp_read_write run"
 
 pkill mysqld
--- demo/sort/build.sh
+++ demo/sort/build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # build时间如果太短，可能导致check_success误报
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
-gcc -O2 -o /home/lyc/gitee/A-FOT/demo/sort/test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
+gcc -O2 -o ./test sort.c
